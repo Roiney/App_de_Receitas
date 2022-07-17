cabf1661180d428e00f599e88aba4c98ff670153
Thiago Gasparini <thiago_engprod@outlook.com>
2022-07-17 01:20:16 -0300

    requisitos 19 ao 23 quase concluidos

--- src/context/RecProvider.js
+++ src/context/RecProvider.js
@@ -12,6 +12,26 @@ export default function RecProvider({ children }) {
   const [drink, setDrink] = useState([]);
   const [foodsIn12, setFoodsIn12] = useState([]);
   const [drinksIn12, setDrinksIn12] = useState([]);
+  const [btnFoods, setBtnFoods] = useState([]);
+  const [btnDrinks, setBtnDrinks] = useState([]);
+  const [filterCategory, setFilterCategory] = useState([]);
+  const [filterCategoryDrink, setFilterCategoryDrink] = useState([]);
+
+  const reqApiCategoryDrink = async (category) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    setFilterCategoryDrink(data.drinks);
+    console.log((data.drinks).slice(0, +'12'));
+  };
+
+  const reqApiCategory = async (category) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    setFilterCategory(data.meals);
+    console.log((data.meals).slice(0, +'12'));
+  };
 
   const reqApiFoods = async () => {
     const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
@@ -27,6 +47,22 @@ export default function RecProvider({ children }) {
     const data = await result.json();
     setDrinksIn12(data.drinks);
     console.log(drinksIn12);
+  };
+
+  const reqApiBtnFoods = async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+    const result = await fetch(url);
+    const data = await result.json();
+    setBtnFoods(data.meals);
+    console.log(btnFoods);
+  };
+
+  const reqApiBtnDrinks = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+    const result = await fetch(url);
+    const data = await result.json();
+    setBtnDrinks(data.drinks);
+    console.log(btnDrinks);
   };
 
   const setFetch = (apiReq) => {
@@ -53,6 +89,16 @@ export default function RecProvider({ children }) {
     foodsIn12,
     drinksIn12,
     reqApiDrinks,
+    btnFoods,
+    reqApiBtnFoods,
+    btnDrinks,
+    reqApiBtnDrinks,
+    filterCategory,
+    reqApiCategory,
+    filterCategoryDrink,
+    reqApiCategoryDrink,
+    setFilterCategoryDrink,
+    setFilterCategory,
   };
 
   return (
--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import contexto from '../context';
@@ -10,27 +11,79 @@ export default function Drinks(props) {
   const cont = useContext(contexto);
   const { context } = cont;
 
-  const { drink, reqApiDrinks, drinksIn12 } = context;
+  const { drink, reqApiDrinks, drinksIn12, btnDrinks,
+    reqApiBtnDrinks, filterCategoryDrink, reqApiCategoryDrink,
+    setFilterCategoryDrink } = context;
 
   useEffect(() => {
     reqApiDrinks();
-  });
+  }, []);
+
+  useEffect(() => {
+    reqApiBtnDrinks();
+  }, []);
+
+  const handleCategoryFilter = (category) => category
+    .slice(0, +'12').map((item, index) => (
+      <Link
+        to={ `/drinks/${item.idDrink}` }
+        key={ item.idDrink }
+        data-testid={ `${index}-recipe-card` }
+      >
+        <div>
+          <p data-testid={ `${index}-card-name` }>{ item.strDrink }</p>
+          <img
+            src={ item.strDrinkThumb }
+            alt=""
+            className="imageItem"
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      </Link>
+    ));
 
   return (
     <div>
       <Header searchIcon="visible" title="Drinks" history={ history } />
-      Profile
-      {(drink.length ? drink : drinksIn12).slice(0, +'12').map((item, index) => (
-        <div key={ index } data-testid={ `${index}-recipe-card` }>
-          <p data-testid={ `${index}-card-name` }>{item.strDrink}</p>
-          <img
-            data-testid={ `${index}-card-img` }
-            src={ item.strDrinkThumb }
-            alt=""
-            className="imageItem"
-          />
-        </div>
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ () => setFilterCategoryDrink([]) }
+      >
+        All
+      </button>
+      {btnDrinks.slice(0, +'5').map((button, index) => (
+        <span key={ index }>
+          <button
+            type="button"
+            data-testid={ `${button.strCategory}-category-filter` }
+            onClick={ () => reqApiCategoryDrink(button.strCategory) }
+          >
+            {button.strCategory}
+          </button>
+        </span>
       ))}
+      {filterCategoryDrink.length
+        ? handleCategoryFilter(filterCategoryDrink)
+        : (drink.length ? drink : drinksIn12)
+          .slice(0, +'12')
+          .map((item, index) => (
+            <Link
+              to={ `/drinks/${item.idDrink}` }
+              key={ index }
+              data-testid={ `${index}-recipe-card` }
+            >
+              <div>
+                <p data-testid={ `${index}-card-name` }>{item.strDrink}</p>
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ item.strDrinkThumb }
+                  alt=""
+                  className="imageItem"
+                />
+              </div>
+            </Link>
+          ))}
       <Footer history={ history } />
     </div>
   );
