977845f0f1e6e8c1df1dbbf50aff655a77a892a3
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-07-18 15:53:37 -0300

    Requisitos de 57 a 62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,11 +8,39 @@ export default function Profile() {
   const buttonBackFoods = () => {
     history.push('/foods');
   };
+
+  const localStorageReset = () => {
+    history.push('/');
+    localStorage.clear();
+  };
+
+  const email = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <button type="button" onClick={ buttonBackFoods }>Tela principal</button>
       <Header searchIcon="hidden" title="Profile" history={ history } />
-      Profile
+      <p data-testid="profile-email">{ email.email }</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ localStorageReset }
+      >
+        Logout
+      </button>
       <Footer history={ history } />
     </div>
   );
