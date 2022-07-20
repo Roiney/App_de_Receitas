67c33c3c44b3956b3e054e7a58141b2a6e25f2a1
Roiney Beal <nei_beal@hotmail.com>
2022-07-20 16:24:47 -0300

    teste

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -4,6 +4,8 @@ import Header from '../components/Header';
 
 export default function FavoriteRecipes(props) {
   const { history } = props;
+  console.log(JSON.parse(localStorage.getItem('favoriteRecipes')));
+
   return (
     <div>
       <Header searchIcon="hidden" title="Favorite Recipes" history={ history } />
